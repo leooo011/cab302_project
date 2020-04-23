0f1fe3a24eea21ca90393e638276281c0db66194
HH <H29112000@gmail.com>
2020-04-23 12:41:54 +1000

    Database connect to user class

---
+++ db.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE IF NOT EXISTS db;
+
+USE db;
+
+DROP TABLE  IF EXISTS `db`.`users`;
+
+CREATE TABLE IF NOT EXISTS `db`.`users`(
+  `userName` varchar(30) NOT NULL,
+  `hashedPassword2nd` varchar(255) NOT NULL,
+  `salt` varchar(50) NOT NULL,
+  `editUsers` int,
+  `editAllBillboard` int,
+  `createBillboard` int,
+  `scheduleBillboard` int,
+  PRIMARY KEY(`accountName`)
+)ENGINE=MyISAM DEFAULT CHARSET=latin1;
