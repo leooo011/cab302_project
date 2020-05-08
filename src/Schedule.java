891fbe7af8ee24b187ddc58878ab04c37b206527
HH <H29112000@gmail.com>
2020-05-09 06:35:49 +1000

    init schedule class

---
+++ src/Schedule.java
@@ -0,0 +1,23 @@
+import java.io.File;
+import java.util.Date;
+
+public class Schedule {
+    private Date date;
+    private Date time;
+    private User user;
+    private Billboard billboard;
+    public Schedule(User user, Billboard billboard, Date date, Date time){
+        this.billboard= billboard;
+        this.date = date;
+        this.time = time;
+        this.user = user;
+    }
+
+    public Date getBillboardDate(){return date;}
+    public Date getBillboardTime(){return time;}
+    public void setDate(Date date){this.date = date;}
+    public void setTime(Date time){this.time = time;}
+    public void setBillboard(Billboard billboard){this.billboard = billboard;}
+    public Date getDate(){return date;}
+    public Date getTime(){return time;}
+}
