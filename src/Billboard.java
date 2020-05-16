aa08dc1e7c9c65d58f5796d0fe848af5a4cf9bcf
HH <H29112000@gmail.com>
2020-05-16 21:36:42 +1000

    update billboard class

--- src/Billboard.java
+++ src/Billboard.java
@@ -2,15 +2,15 @@ import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.xml.sax.SAXException;
-
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
 import java.io.IOException;
+import java.io.Serializable;
 import java.util.HashMap;
 
-public class Billboard {
+public class Billboard implements Serializable {
     private String userName;
     private String billboardName;
     private String billboardBackground;
