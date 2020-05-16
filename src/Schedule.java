b6963d2c97d2bc4f2985574fd122045f1e8b5369
HH <H29112000@gmail.com>
2020-05-16 21:38:07 +1000

    update schedule class

--- src/Schedule.java
+++ src/Schedule.java
@@ -1,19 +1,22 @@
+import java.io.Serializable;
 import java.util.Date;
 
-public class Schedule {
+public class Schedule implements Serializable {
     private Date date;
     private Date time;
     private User user;
     private Billboard billboard;
-    private Date duration;
-    private Date recurTime;
+    private Date duration = Server.parseTime("00:00:00");
+    private Date recurTime= Server.parseTime("00:00:00");
     public Schedule(User user, Billboard billboard, Date date, Date time, Date recurTime,Date duration){
         this.billboard= billboard;
         this.date = date;
         this.time = time;
         this.user = user;
-        this.duration= duration;
-        this.recurTime =recurTime;
+        if(duration!=null){
+        this.duration= duration;}
+        if(recurTime!=null){
+        this.recurTime =recurTime;}
     }
     public Date getDuration(){return duration;}
     public Date getRecurTime(){return recurTime;}
